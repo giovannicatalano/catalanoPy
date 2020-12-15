13bf0c8f4d65c9b5abde3d2cbcb108a151c4869b
giovannicatalano <75336569+giovannicatalano@users.noreply.github.com>
2020-12-15 19:21:21 +0100

    Add files via upload

---
+++ equazione.py
@@ -0,0 +1,14 @@
+import math
+print ("ax+b=0??")
+a = int (input ("valore della incognita a?")
+b = int (input ("valore della incognita b?")
+print("x=b:a")
+x = (-b/a)
+print (x)
+max (a,b,x)
+print ("Il numero maggiore tra a,b e x è...")
+print (max(a,b,x))
+def swap (a,b)
+    a,b=b,a
+print ("a è uguale a ',a,' e b è uguale a',b")
+swap (a,b)
---
+++ grafico prova.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([19, -6])
+ypoints = np.array([4, -7])
+plt.plot(xpoints, ypoints)
+plt.show()
