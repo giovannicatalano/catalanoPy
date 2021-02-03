9cbcc98e74ae086a13a2a49da40df6ed5b5501ff
giovannicatalano <75336569+giovannicatalano@users.noreply.github.com>
2021-02-03 09:40:42 +0100

    Delete stringhe contalettere.py

--- stringhe contalettere.py
+++
@@ -1,7 +0,0 @@
-stringa = input('per favore scrvi una stringa')
-lista_lettere = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-print('I numeri delle lettere che si ripetono sono:')
-stringa = list(stringa.upper())
-for letter in lista_lettere:
-    if stringa.count(lettera)>0:
-        print(lettera, ' = ',stringa.count(lettera))
