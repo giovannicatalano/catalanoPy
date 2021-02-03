9ad91b6f17f491e5533a4ea380adf3ae0d334658
giovannicatalano <75336569+giovannicatalano@users.noreply.github.com>
2021-02-03 09:34:17 +0100

    Add files via upload

---
+++ Esercizi/orchestra.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+data = {'Fisarmonica': 10, 'Piano': 4, 'Viola': 18, 'Trombe': 20}
+names = list(data.keys())
+values = list(data.values())
+
+fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+axs[0].bar(names, values)
+axs[1].scatter(names, values)
+axs[2].plot(names, values)
+fig.suptitle('strumenti in orchestra')
+
+
+plt.show()
---
+++ Esercizi/patatine fritte.py
@@ -0,0 +1,8 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> stringa1 = "patatine"
+>>> stringa2 = "fritte"
+>>> stringa3 = " "
+>>> print(stringa1+stringa3+stringa2)
+patatine fritte
+>>> 
